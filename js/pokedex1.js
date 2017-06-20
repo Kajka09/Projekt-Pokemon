9003dde9a5451832d3e004e31ef5e122a8b44cf9
Kajka09 <kajkastefaniak@gmail.com>
2017-06-21 00:16:23 +0200

    table

--- js/pokedex1.js
+++ js/pokedex1.js
@@ -1,30 +1,34 @@
 'use strict';
-$('#getPokemons').click(function () {
-	$.ajax({
-		type: "GET",
-		url: "http://pokeapi.co/pokemon/",
-		dataType: 'json',
-		success: function (response) {
-			var divTable = document.createElement('div');
-			divTable.setAttribute('id', 'tableContainer');
-			
+$('#getPokemons').click(function(){
 			var table = document.createElement('table');
-			table.className = "table table-condensed";
-			table.setAttribute("id", "ajaxTable");
+//			table.className = "table table-condensed";
+//			table.setAttribute("id", "ajaxTable");
+			
+			
 			
 			var header = document.createElement('tr');
-			header.innerHTML = '<th> Image </th><th> Name </th><th> HP </th>';
-			header.setAttribute("id", "tableHeader");
+			header.innerHTML = '<th> Name </th><th> Image </th><th> HP </th>';
+//			header.setAttribute("id", "tableHeader");
 			table.appendChild(header);
 			
-			response.forEach(function (data) {
-				var row = document.createElement('tr');
-				row.innerHTML = '<td>' + data.name+ '</td>';
-				table.append(row);
-			});
+	for(var i = 1; i <= 10; i++){
+	$.ajax({
+		type: "GET",
+		url: "http://pokeapi.co/api/v2/pokemon/"+i+"/",
+		dataType: 'json',
+		success: function(response){
+			var name = response.forms[0].name;
+			var imgUrl = response.sprites.front_default;
+			var hpName = response.stats[5].stat.name ; 
+			var hpVal= response.stats[5].base_stat;
 			
-			divTable.appendChild(table);
-			$('#whiteScreen').append(divTable);
+			var row = document.createElement('tr');
+			row.innerHTML = '<td>' + name + '</td>' + '<td>' + '<img src ="'+imgUrl+'"/>' + '</td>' + '<td>' + hpVal + '</td>';
+			table.append(row);
+			
 		}
 	});
+}
+
+$('#screen').append(table);
 });
