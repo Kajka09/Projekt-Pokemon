ad69b5bf7421fd6410c47b53b00f2bb3d0486fdf
Kajka09 <kajkastefaniak@gmail.com>
2017-06-21 16:11:04 +0200

    2
    image rotation

--- js/pokedex1.js
+++ js/pokedex1.js
@@ -1,34 +1,39 @@
 'use strict';
 $('#getPokemons').click(function(){
-			var table = document.createElement('table');
-//			table.className = "table table-condensed";
-//			table.setAttribute("id", "ajaxTable");
-			
-			
-			
-			var header = document.createElement('tr');
-			header.innerHTML = '<th> Name </th><th> Image </th><th> HP </th>';
-//			header.setAttribute("id", "tableHeader");
-			table.appendChild(header);
-			
-	for(var i = 1; i <= 10; i++){
-	$.ajax({
-		type: "GET",
-		url: "http://pokeapi.co/api/v2/pokemon/"+i+"/",
-		dataType: 'json',
-		success: function(response){
-			var name = response.forms[0].name;
-			var imgUrl = response.sprites.front_default;
-			var hpName = response.stats[5].stat.name ; 
-			var hpVal= response.stats[5].base_stat;
-			
-			var row = document.createElement('tr');
-			row.innerHTML = '<td>' + name + '</td>' + '<td>' + '<img src ="'+imgUrl+'"/>' + '</td>' + '<td>' + hpVal + '</td>';
-			table.append(row);
-			
-		}
-	});
-}
+	var table = document.createElement('table');
+	table.className = "table table-condensed";
+	table.setAttribute("id", "ajaxTable");
 
-$('#screen').append(table);
+	var header = document.createElement('tr');
+	header.innerHTML = '<th> Name </th><th> Image </th><th> HP </th>';
+	header.setAttribute("id", "tableHeader");
+	table.appendChild(header);
+
+	var random = Math.floor(Math.random()*100);
+	for(var i = random ; i <= random + 10; i++){
+		$.ajax({
+			type: "GET",
+			url: "https://pokeapi.co/api/v2/pokemon/"+i+"/",
+			dataType: 'json',
+			success: function(response){
+				var name = response.forms[0].name;
+				var imgUrl = response.sprites.front_default;
+				var hpName = response.stats[5].stat.name ; 
+				var hpVal= response.stats[5].base_stat;
+
+				var row = document.createElement('tr');
+				row.innerHTML = '<td>' + name + '</td>' + '<td>' + '<img id="pokeImage" src ="'+imgUrl+'" />' + '</td>' + '<td>' + hpVal + '</td>';
+				$(document).on("mouseover", "#pokeImage", rotateImage);
+				$(document).on("mouseout", "#pokeImage", reRotateImage);
+				$('#pokedex1').append(row);
+			}
+		});
+	}    
+	$('#pokedex1').append(table);
 });
+function rotateImage(event) {
+	$(event.target).css('transform', 'rotate(' + 1000 + 'deg)');
+}
+function reRotateImage(event) {
+	$(event.target).css('transform', 'rotate(' + 0 + 'deg)');
+}
